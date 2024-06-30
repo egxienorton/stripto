6745401c7bd8d77db572bbe2c4d2e08c2a61cad6
egxienorton <53386999+egxienorton@users.noreply.github.com>
2024-06-30 22:08:16 +0100

    Fleek updates

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
   basePath: "",
   eslint: {
     ignoreDuringBuilds: true,
   },
+  
 };
 
 module.exports = nextConfig;
